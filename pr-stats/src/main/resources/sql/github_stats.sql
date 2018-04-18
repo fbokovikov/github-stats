b9558ee7782b861705a607fc6157c74d77e1f232
fbokovikov <fbokovikov@yandex-team.ru>
2018-04-18 15:35:23 +0300

    Добавил скрипты по созданию пользователя и схемы

---
+++ pr-stats/src/main/resources/sql/github_stats.sql
@@ -0,0 +1,17 @@
+CREATE USER github_stats IDENTIFIED BY github_stats;
+
+ALTER USER github_stats quota 100M on system;
+
+GRANT CREATE SESSION TO github_stats;
+
+GRANT CREATE TABLE TO github_stats;
+
+CREATE TABLE github_stats.github_branch (
+    branch_name VARCHAR2(100 CHAR),
+    branch_owner VARCHAR2(40 CHAR),
+    update_datetime TIMESTAMP,
+    repository_name VARCHAR2(100 CHAR)
+);
+
+ALTER TABLE github_stats.github_branch
+ADD CONSTRAINT github_branch_pk PRIMARY KEY (branch_name);
