84862348493b7b77574665730e77ecea603d0fb2
fbokovikov <fbokovikov@yandex-team.ru>
2018-04-20 19:54:11 +0300

    Добавил Sample получения коммитов из ветки

---
+++ pr-stats/src/test/java/ru/yandex/market/github/pr/stats/service/CommitServiceTest.java
@@ -0,0 +1,39 @@
+package ru.yandex.market.github.pr.stats.service;
+
+import org.eclipse.egit.github.core.IRepositoryIdProvider;
+import org.eclipse.egit.github.core.RepositoryCommit;
+import org.eclipse.egit.github.core.client.PageIterator;
+import org.eclipse.egit.github.core.service.CommitService;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.yandex.market.github.pr.stats.config.FunctionalTest;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author fbokovikov
+ */
+public class CommitServiceTest extends FunctionalTest {
+
+    private static final IRepositoryIdProvider MBI_REPO = () -> "market-java/mbi";
+
+    @Autowired
+    private CommitService commitService;
+
+    @Test
+    public void getMasterCommits() throws IOException {
+        List<RepositoryCommit> commits = new ArrayList<>();
+        PageIterator<RepositoryCommit> commitsIterator = commitService.pageCommits(MBI_REPO, 100);
+        while (commitsIterator.hasNext() && commits.size() < 500) {
+            commits.addAll(commitsIterator.next());
+        }
+        MatcherAssert.assertThat(
+                commits,
+                Matchers.hasSize(500)
+        );
+    }
+}
